89963433f9526f9650cb3e04134b26554e6f9158
caltonji <caltonji@gmail.com>
2024-02-13 22:46:11 -0800

    Update headers to avoid github action timeout

--- nba-bets/download-game-logs.py
+++ nba-bets/download-game-logs.py
@@ -5,7 +5,23 @@ import pandas as pd
 
 def main():
     print("Downloading Game Logs")
-    game_logs = playergamelogs.PlayerGameLogs(season_nullable="2023-24", timeout=120)
+    custom_headers = {
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Host': 'stats.nba.com',
+        'Origin': 'https://www.nba.com',
+        'Referer': 'https://www.nba.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'sec-ch-ua': '"Not A Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"'
+    }
+    game_logs = playergamelogs.PlayerGameLogs(season_nullable="2023-24", headers=custom_headers)
     df = game_logs.get_data_frames()[0]
     print(f"Found {len(df)} rows")
     # Connect to SQLite database
