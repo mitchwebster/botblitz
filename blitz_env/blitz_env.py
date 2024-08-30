615a0d87aa39aa5df7c5fb517708b0edb4ee3be6
Mitch Webster <mitchwebster@github.com>
2024-08-29 19:19:03 -0700

    Add function for getting the current round

--- blitz_env/blitz_env.py
+++ blitz_env/blitz_env.py
@@ -1,5 +1,6 @@
 import os
 from .loadPlayers import load_all_players
+from .agent_pb2 import GameState
 
 def load_players(year: int):
     if year not in [2023, 2024]:
@@ -8,3 +9,7 @@ def load_players(year: int):
     file_path = os.path.join(current_dir, f'player_ranks_{str(year)}.csv')
     players = load_all_players(file_path)
     return players
+
+def get_current_round(game_state: GameState) -> int:
+    zero_based_round = (game_state.current_pick - 1) // len(game_state.teams)
+    return zero_based_round + 1
