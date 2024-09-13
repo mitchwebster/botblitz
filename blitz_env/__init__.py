e79d5e924fd9125dc2837860cb8d187092109d21
Mitch Webster <mitchwebster@github.com>
2024-09-12 23:07:39 -0700

    Update GRPC tools and merge proto tools

--- blitz_env/__init__.py
+++ blitz_env/__init__.py
@@ -1,4 +1,5 @@
 from .blitz_env import load_players
 from .stats_db import StatsDB
 from .simulate_draft import is_drafted, simulate_draft, visualize_draft_board
-from .agent_pb2 import Player, GameState
+from .agent_pb2 import Player, GameState, DraftSelection
+from .agent_pb2_grpc import AgentServiceServicer, add_AgentServiceServicer_to_server
