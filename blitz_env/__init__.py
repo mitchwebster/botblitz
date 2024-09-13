d087ee2731e8effbcd1c420adc8409080be16d0c
Mitch Webster <mitchwebster@github.com>
2024-09-12 23:33:27 -0700

    Update wheels

--- blitz_env/__init__.py
+++ blitz_env/__init__.py
@@ -1,7 +1,5 @@
 from .blitz_env import load_players
 from .stats_db import StatsDB
 from .simulate_draft import is_drafted, simulate_draft, visualize_draft_board
-import agent_pb2
 from .agent_pb2 import Player, GameState, DraftSelection
 from .projections_db import ProjectionsDB
-from .agent_pb2_grpc import AgentServiceServicer, add_AgentServiceServicer_to_server
