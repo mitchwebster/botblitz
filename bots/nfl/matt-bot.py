46b66225d821df5c3be2bcc141bf8ccba1ece06b
Matt Henricks <mchenricks21@gmail.com>
2024-09-14 14:09:36 -0700

    use floats instead of int. update weights. comment

--- bots/nfl/matt-bot.py
+++ bots/nfl/matt-bot.py
@@ -1,6 +1,7 @@
 from blitz_env import is_drafted, simulate_draft, visualize_draft_board, Player, GameState
 from typing import List
 
+# Drafted players for my team
 drafted_wrs = []
 drafted_rbs = []
 drafted_tes = []
@@ -8,6 +9,7 @@ drafted_qbs = []
 drafted_ks = []
 drafted_dsts = []
 
+# Max number of positions to draft
 NUM_MAX_WRS = 5
 NUM_MAX_RBS = 5
 NUM_MAX_TES = 1 # TODO probably drop this to 1?
@@ -15,13 +17,13 @@ NUM_MAX_QBS = 2
 NUM_MAX_KS = 1
 NUM_MAX_DSTS = 1
 
-# TODO confirm these from CSV. Gotten from FanstasyPros
+# Max number of positions based on CSV for 2024 projected players
 MAX_POS_RANK_WR = 200
 MAX_POS_RANK_RB = 156
 MAX_POS_RANK_TE = 97
 MAX_POS_RANK_QB = 67
 MAX_POS_RANK_K = 34
-MAX_POS_RANK_DST = 31   # isn't this 32?
+MAX_POS_RANK_DST = 32
 MAX_RANK_ALL_PLAYERS = 586
 
 def draft_player(game_state: GameState) -> str:
@@ -40,6 +42,7 @@ def draft_player(game_state: GameState) -> str:
 
     # Select the player with the highest rank (lowest rank number)
     if undrafted_players:
+        # Default boilerplate
         # drafted_player = min(undrafted_players, key=lambda p: p.rank)
         # drafted_player = undrafted_players.sorted(undrafted_players, key=lambda player:player.)
 
@@ -61,7 +64,7 @@ def experiment(undrafted_players: List[Player], drafted_players: List[Player]):
   undrafted_players = [p for p in undrafted_players if can_draft_position(p)]
 
   # Sort players according to scoring function
-  players_scored = [] #dict({})
+  players_scored = []
   for player in undrafted_players:
     player_score = score_player(player, undrafted_players, drafted_players)
     scored_player = ScoredPlayer(player, player_score)
@@ -73,29 +76,29 @@ def experiment(undrafted_players: List[Player], drafted_players: List[Player]):
 
   print(list(map(lambda p: str(p.player.full_name) + " | score: " + str(p.score) + " | need: " + str(get_position_need(p.player)) + " | demand: " + str(get_position_demand(p.player, drafted_players)), players_ranked)))
 
-  # best_player = min(players_ranked, key=lambda p: p.rank * p.position_rank);
+  # Get best Player object
   best_player = players_ranked[0].player
 
+  # Store the player
   store_player(best_player)
 
   return best_player
 
 def score_player(player: Player, undrafted_players: List[Player], drafted_players: List[Player]):
+  # Default boilerplate
   # return player.rank
   # return player.rank * player.position_rank
 
   # This worked okay
   # position_multiplier = 1 if (is_wr(player) or is_rb(player)) and player.position_tier == 1 else 2
   # score = (player.rank - player.position_rank) * position_multiplier
-
   # score = player.rank * 0.3 + player.position_rank * 0.4 + position_need * 0.3
 
-  # This worked really well
+  # This worked well
   # score = get_rank_percentile(player) * 0.3 + get_position_rank_percentile(player) * 0.4 + get_position_need(player) * 0.3
 
-  score = get_rank_percentile(player) * 0.35 + get_position_rank_percentile(player) * 0.35 + get_position_need(player) * 0.29 + get_position_demand(player, drafted_players) * 0.01
-
-  # print(str(player.full_name) + " | need: " + str(position_need))
+  # Weighted average for player score
+  score = get_rank_percentile(player) * 0.35 + get_position_rank_percentile(player) * 0.35 + get_position_need(player) * 0.285 + get_position_demand(player, drafted_players) * 0.015
 
   return score
 
@@ -159,10 +162,7 @@ def get_position_demand(player: Player, drafted_players: List[Player]):
 
   average_position_rank_drafted = total_position_rank / get_max_players_for_position(player)
 
-
   return average_position_rank_drafted
-
-
 
 def can_draft_position(player: Player):
   match player.allowed_positions[0]:
@@ -224,8 +224,10 @@ def store_player(player: Player):
       drafted_dsts.append(player)
       return
 
-
+# Class for containing the Player with their corresponding score
 class ScoredPlayer:
-  def __init__(self, player: Player, score: int):
+  def __init__(self, player: Player, score: float):
       self.player = player
       self.score = score
+
+game_state = simulate_draft(draft_player, 2024)
