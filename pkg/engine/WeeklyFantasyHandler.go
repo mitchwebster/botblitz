3636bef50317cc69cb7190eba817bdae4ef90647
Mitch Webster <mitchwebster@github.com>
2024-09-28 10:30:04 -0700

    Add initial waiver order logic

--- pkg/engine/WeeklyFantasyHandler.go
+++ pkg/engine/WeeklyFantasyHandler.go
@@ -3,18 +3,22 @@ package engine
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"sort"
 
 	common "github.com/mitchwebster/botblitz/pkg/common"
 )
 
 const AvailableMapKey = "AVAILABLE"
+const AllowedAddsPerRun = 3
 
 func (e *BotEngine) runWeeklyFantasy(ctx context.Context) error {
-	fmt.Println("Playing Weekly Fantasy!!")
+	err := performWeeklyFantasyActions(e)
+	if err != nil {
+		return err
+	}
 
-	_ = buildTeamMap(e.gameState)
-
-	err := SaveGameState(e.gameState)
+	err = SaveGameState(e.gameState)
 	if err != nil {
 		return err
 	}
@@ -27,30 +31,94 @@ func (e *BotEngine) runWeeklyFantasy(ctx context.Context) error {
 	return nil
 }
 
-func buildTeamMap(gameState *common.GameState) map[string][]*common.Player {
-	playerMap := make(map[string][]*common.Player)
+func performWeeklyFantasyActions(e *BotEngine) error {
+	fmt.Println("Playing Weekly Fantasy!!")
 
-	for _, player := range gameState.Players {
-		teamKey := player.Status.CurrentFantasyTeamId
-		if teamKey == "" && player.Status.Availability == common.PlayerStatus_AVAILABLE {
-			teamKey = AvailableMapKey
-		}
+	makePreviouslyOnHoldPlayersAvailable(e.gameState)
 
-		value, exists := playerMap[teamKey]
-		if exists {
-			appendedList := append(value, player)
-			playerMap[teamKey] = appendedList
-		} else {
-			playerMap[teamKey] = []*common.Player{player}
-		}
+	err := setupInitialWaiverPriorityIfNeeded(e.gameState)
+	if err != nil {
+		return err
 	}
 
-	for team, playerList := range playerMap {
-		if team == AvailableMapKey {
-			continue
-		}
-		fmt.Printf("Key: %s, Value: %s\n", team, playerList)
+	// Collect the desired actions from the bots
+
+	// Go through them in the waiver priority order
+	sort.Slice(e.gameState.Teams, func(i, j int) bool {
+		return e.gameState.Teams[i].CurrentWaiverPriority < e.gameState.Teams[j].CurrentWaiverPriority
+	})
+
+	for _, team := range e.gameState.Teams {
+		fmt.Println(team.Id)
+		fmt.Println(team.Name)
+		fmt.Println(team.CurrentWaiverPriority)
 	}
 
-	return playerMap
+	// playerMap := buildTeamMap(e.gameState)
+	return nil
 }
+
+func makePreviouslyOnHoldPlayersAvailable(gameState *common.GameState) {
+	for _, player := range gameState.Players {
+		if player.Status.Availability == common.PlayerStatus_ON_HOLD {
+			player.Status.Availability = common.PlayerStatus_AVAILABLE
+			fmt.Println(player.FullName + " is now available!")
+		}
+	}
+}
+
+func setupInitialWaiverPriorityIfNeeded(gameState *common.GameState) error {
+	// check if all of the teams are missing a waiver priority
+	for _, team := range gameState.Teams {
+		if team.CurrentWaiverPriority != 0 {
+			return nil
+		}
+	}
+
+	fmt.Println("Initializing team waiver priorities")
+	n := len(gameState.Teams)
+	indices := make([]int, 0, n)
+
+	for i := 0; i < n; i++ {
+		indices = append(indices, i)
+	}
+
+	rand.Shuffle(len(indices), func(i, j int) {
+		indices[i], indices[j] = indices[j], indices[i]
+	})
+
+	for i := 0; i < n; i++ {
+		curTeam := gameState.Teams[i]
+		curTeam.CurrentWaiverPriority = uint32(indices[i])
+	}
+
+	return nil
+}
+
+// func buildTeamMap(gameState *common.GameState) map[string][]*common.Player {
+// 	playerMap := make(map[string][]*common.Player)
+
+// 	for _, player := range gameState.Players {
+// 		teamKey := player.Status.CurrentFantasyTeamId
+// 		if teamKey == "" && player.Status.Availability == common.PlayerStatus_AVAILABLE {
+// 			teamKey = AvailableMapKey
+// 		}
+
+// 		value, exists := playerMap[teamKey]
+// 		if exists {
+// 			appendedList := append(value, player)
+// 			playerMap[teamKey] = appendedList
+// 		} else {
+// 			playerMap[teamKey] = []*common.Player{player}
+// 		}
+// 	}
+
+// 	for team, playerList := range playerMap {
+// 		if team == AvailableMapKey {
+// 			continue
+// 		}
+// 		fmt.Printf("Key: %s, Value: %s\n", team, playerList)
+// 	}
+
+// 	return playerMap
+// }
